6c23614090fa17a383a8f8ceef311910eba5fd3f
unknown <etienne.orieux.etu@univ-lille.fr>
2019-10-28 19:46:11 +0100

    the opponent doesn't know where the ennemy hidden queen is

--- client/js/client.js
+++ client/js/client.js
@@ -17,6 +17,65 @@ function onDragStart (source, piece, position, orientation) {
   if (game.game_over()) return false
 }
 
+const writeEvent = (text) => {
+  console.log(text);
+}
+
+const createClient = (color) => {
+  player = new Player("Name", color.charAt(0));
+  setUpBoard(color);
+}
+
+const setUpBoard = (color) => {
+  var config = {
+    draggable: true,
+    position: 'start',
+    onDragStart: onDragStart,
+    onDrop: onDrop,
+    orientation: color,
+    pieceTheme:getPieceTheme,
+    // pieceTheme:'img/chesspieces/wikipedia/{piece}.png'
+  }
+  board = ChessBoard('board', config);
+  board.position();
+  writeEvent(`Vous avez les ${color}`);
+}
+
+const buildPieceFromString = (pieceString) => {
+  piece = {
+    color:pieceString[0],
+    type:pieceString[1],
+  }
+  return piece;
+}
+
+const onHqChoice = (piece, square) => {
+  if(piece[1] == 'P'){
+    sock.emit('putHq', {color:player.getColor(), square:square});
+  }else{
+    writeEvent("Mauvaise Pièce !")
+  }
+}
+
+function getPieceTheme(pieceString){
+
+  defaultPieceDir = 'wikipedia';
+  hiddenQueenDir = 'hidden_queen';
+
+  piece = buildPieceFromString(pieceString)
+  let pieceDir = defaultPieceDir;
+  if (piece.type == "H" && piece.color == player.getColor()){
+    pieceDir = hiddenQueenDir;
+  }
+
+  return 'img/chesspieces/' + pieceDir + '/' + pieceString +'.png'
+}
+
+const createGame = (message) => {
+  game = new Chess()
+  writeEvent(message);
+}
+
 function onDrop (source, target) {
   // see if the move is legal
   var move = game.move({
@@ -36,44 +95,9 @@ function updateBoardPosition(){
   board.position(game.fen())
 }
 
-const writeEvent = (text) => {
-  console.log(text);
-}
-
-const createClient = (color) => {
-  player = new Player("Name", color.charAt(0));
-  setUpBoard(color);
-}
-
-const setUpBoard = (color) => {
-  var config = {
-    draggable: true,
-    position: 'start',
-    onDragStart: onDragStart,
-    onDrop: onDrop,
-    orientation: color
-  }
-  board = ChessBoard('board', config);
-  board.position();
-  writeEvent(`Vous avez les ${color}`);
-}
-
 const putHq = (hq) => {
   game.put({ type: game.HIDDEN_QUEEN, color: hq.color }, hq.square);
   updateBoardPosition();
-}
-
-const onHqChoice = (piece, square) => {
-  if(piece.includes("P")){
-    sock.emit('putHq', {color:player.getColor(), square:square});
-  }else{
-    writeEvent("Mauvaise Pièce !")
-  }
-}
-
-const createGame = (message) => {
-  game = new Chess()
-  writeEvent(message);
 }
 
 const makeMove = (move) => {
