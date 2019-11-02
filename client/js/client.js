d7a5ec3aa4fffbafa7b9e909261ca012304cfeb7
orieuxe <etienne.orieux.etu@univ-lille.fr>
2019-11-02 16:34:55 +0100

    the players choose their opponent before starting a match

--- client/js/client.js
+++ client/js/client.js
@@ -22,3 +22,25 @@ Chatbox.writeEvent('Bienvenue sur Dames Cachées')
 sock.on('message', Chatbox.writeEvent);
 sock.on('color', createClient);
 sock.on('gameStarts', createGame);
+
+var $playerList = $("#playerList")
+
+const showAllClients = (clients) => {
+  $playerList.empty();
+  clients.forEach((client) => {
+    //doesn't show own client
+    if (client != sock.id){
+      html = `<button id=${client} class="client">${client}</button>`;
+      $playerList.append(html);
+    }
+  })
+}
+
+//New match
+$playerList.on('click',".client",(event) => {
+  let opponentId = event.target.id
+  sock.emit('opponentClick', opponentId);
+});
+
+//New client / client quitting lobby
+sock.on('clientsChange', showAllClients);
--- server/server.js
+++ server/server.js
@@ -13,23 +13,29 @@ app.use(express.static(clientPath));
 const server = http.createServer(app);
 const io = socketio(server);
 
-let waitingSocket = null;
+var updateClientList = (namespace) => {
+  namespace.clients((error, clients) => {
+    if (error) throw error;
+    namespace.emit("clientsChange", clients);
+  })
+}
 
 io.on('connection', (sock) => {
-  if(waitingSocket){
-    //start a game
-    const waiterIsWhite = true;
-    new HqGameRoom(waitingSocket, sock, waiterIsWhite)
-    waitingSocket = null;
-  }else{
-    waitingSocket = sock;
-    waitingSocket.emit('message', "En attente d'un adversaire");
-  }
-})
+  const lobbyNamespace = io.of('/')
+  updateClientList(lobbyNamespace);
+  sock.on('disconnect', () => {
+    updateClientList(lobbyNamespace);
+  })
+
+  sock.on('opponentClick', (opponentId) => {
+    opponent = io.sockets.connected[opponentId];
+    new HqGameRoom(sock, opponent, true);
+  })
+});
 
 server.on('error', (err) =>{
   console.error('Server error:', err);
-})
+});
 
 server.listen(8080, () => {
   console.log('HQ started on 8080');
